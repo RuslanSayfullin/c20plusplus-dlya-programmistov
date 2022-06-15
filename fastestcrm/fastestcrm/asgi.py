153c0804514da576ad9b0cff4efdce43200fab7f
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-06-16 00:05:24 +0500

    Добавили первое приложение

--- fastestcrm/fastestcrm/asgi.py
+++ fastestcrm/fastestcrm/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fastescrm project.
+ASGI config for fastestcrm project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fastescrm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fastestcrm.settings')
 
 application = get_asgi_application()

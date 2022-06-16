10f2377c8aec0b807bb47aaf38b197d903af703f
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-06-16 16:34:16 +0500

    Добавили базовые шаблоны

--- fastestcrm/fastestcrm/urls.py
+++ fastestcrm/fastestcrm/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inquery/', include('inquery.urls')),
 ]
 
 if settings.DEBUG:
@@ -14,3 +16,7 @@ if settings.DEBUG:
     ] + urlpatterns
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Обработка ошибок
+handler404 = TemplateView.as_view(template_name="fastestcrm/404.html")
+handler403 = TemplateView.as_view(template_name="fastestcrm/403.html")
